e62e313d06b21d7fcde924b6cbd60f8845a81a41
Edder Luan <eluann100@gmail.com>
2022-12-29 14:14:25 -0300

    Tratando erros no login
    - 401
    - 403
    - 404

--- frontend/src/app/home/perfil/perfil.component.ts
+++ frontend/src/app/home/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClienteDTO } from 'src/app/model/cliente.dto';
 import { ClienteService } from 'src/app/service/cliente.service';
 import { StorageService } from 'src/app/service/storage.service';
@@ -12,14 +13,23 @@ export class PerfilComponent implements OnInit {
 
   cliente: ClienteDTO;
 
-  constructor(public storage: StorageService, public clienteService: ClienteService) { }
+  constructor(
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public router : Router) { }
 
   ngOnInit() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
      this.clienteService.findByEmail(localUser.email).subscribe(response => {
       this.cliente = response
+     }, error => {
+      if (error.status == 403) {
+        this.router.navigate(['/']);
+      }
      });
+    }else{
+      this.router.navigate(['/']);
     }
   }
 }
--- frontend/src/app/interceptor/error-interceptor.ts
+++ frontend/src/app/interceptor/error-interceptor.ts
@@ -2,29 +2,63 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { StorageService } from '../service/storage.service';
+import { LocalUser } from '../model/local_user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorInterceptor implements HttpInterceptor {
 
+  constructor(private storageService: StorageService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-        catchError((error: HttpErrorResponse) => {
-          let errorMsg = 'Erro aqui' + error;
-          if (error.error instanceof ErrorEvent) {
-            console.log('this is client side error');
-            errorMsg = `Error: ${error.error.message}`;
-          }
-          else {
-            console.log('this is server side error');
-            errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
-          }
-          console.log(errorMsg);
-          return throwError(errorMsg);
-        })
-      )
+      catchError((error: HttpErrorResponse) => {
+        let errorMsg = 'Erro aqui' + error;
+        if (error.error instanceof ErrorEvent) {
+          console.log('this is client side error');
+          errorMsg = `Error: ${error.error.message}`;
+        }
+        else {
+          console.log('this is server side error');
+          errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
+        }
+        console.log(errorMsg);
+
+        switch (error.status) {
+          case 401:
+            this.handle401();
+            break;
+
+          case 403:
+            this.handle403();
+            break;
+
+          default:
+            this.handleDefaultError(errorMsg);
+        }
+        return throwError(errorMsg);
+      })
+    )
   }
+
+  handle401() {
+    alert('Login e senha incorretos');
+  }
+
+  handle403() {
+    let user : LocalUser = {
+      token: '',
+      email: ''
+    }
+    this.storageService.setLocalUser(user);
+  }
+
+  handleDefaultError(errorMsg: any){
+    alert('404: ' + errorMsg.status);
+  }
+
 }
 
 export const ErrorInterceptorProvider = {
